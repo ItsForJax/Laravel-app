58a1ab6ba740fa7cba336501c960bac091e11301
4jax <dadelos.ddls@gmail.com>
2024-02-02 04:12:01 +0000

    Terms and condition page

---
+++ ideas/resources/views/terms.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.layout')
+
+@section('content')
+
+<h1>Terms and Condition</h1>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste voluptatem dicta aliquam illum architecto reiciendis vel vero delectus, dignissimos eaque dolor, asperiores maiores obcaecati harum distinctio? Culpa dolores eius minima?
+</p>
+
+@endsection
